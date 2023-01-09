c82c3c33a7dc765bd2950fcd2c7cdeffd3c69039
alhatmii <alhatmii.150@gmail.com>
2023-01-09 12:08:45 +0400

    Remove duplicate and count remaining

---
+++ src/DuplicateAndCount.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class DuplicateAndCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		countAndReviewNumbers(new int[] {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, });
+		// Output: There are 5 unique numbers: [1, 2, 3, 4, 5]
+	}
+
+	public static void countAndReviewNumbers(int[] numbers) {
+		// Create a set from the numbers to remove duplicates
+		Set<Integer> uniqueNumbers = new HashSet<>();
+		for (int number : numbers) {
+			uniqueNumbers.add(number);
+		}
+
+		// Get the count of unique numbers
+		int count = uniqueNumbers.size();
+
+		// Print a review of the unique numbers
+		System.out.println("There are " + count + " unique numbers: " + uniqueNumbers);
+	}
+
+}
